e149a6f0d052affc474fc53b2b9fa0ff87aa6b6e
Ajamal <asj@ualberta.ca>
2014-11-14 20:41:34 -0800

    Testing if commit works.

--- UAlbertaBot/src/UAlbertaBot/Source/UAlbertaBotModule.cpp
+++ UAlbertaBot/src/UAlbertaBot/Source/UAlbertaBotModule.cpp
@@ -92,7 +92,7 @@ void UAlbertaBotModule::onEnd(bool isWinner)
 
 void UAlbertaBotModule::onFrame()
 {
-	//BWAPI::Broodwar->printf("Aly-khan!");
+	BWAPI::Broodwar->printf("Aly-khan!350githubversion");
 	if (Options::Modules::USING_UNIT_COMMAND_MGR)
 	{
 		UnitCommandManager::Instance().update();
